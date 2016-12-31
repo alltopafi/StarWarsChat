5b2136fa087730eae87098fcac96f548f30bfab3
alltopafi <alltopafi@gmail.com>
2016-12-30 20:37:31 -0600

    network control and cell design

---
+++ Extensions.swift
@@ -0,0 +1,45 @@
+//
+//  Extensions.swift
+//  starwarschat
+//
+//  Created by Jesse Alltop on 12/30/16.
+//  Copyright © 2016 Jesse Alltop. All rights reserved.
+//
+
+import UIKit
+
+    let imageCache = NSCache<AnyObject, AnyObject>()
+
+extension UIImageView {
+    
+    func loadImageUsingCacheWithUrlString(urlString: String) {
+        
+        self.image = nil
+        
+        if let cachedImage = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = cachedImage
+            return
+        }
+        
+        let url = URL(string: urlString)
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            
+            DispatchQueue.main.async( execute: {
+                
+                if let downloadedImage = UIImage(data: data!) {
+                    imageCache.setObject(downloadedImage, forKey: urlString as AnyObject)
+                    self.image = downloadedImage
+                }
+                
+
+            })
+            
+        }).resume()
+    }
+}
